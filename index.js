35c16db485c6ff9649209e4102daba9fa04a2565
katherinex <kathexio@gmail.com>
2022-05-07 22:53:32 -0500

    Add styles for messages sent and received, and create button test

--- index.js
+++ index.js
@@ -19,10 +19,17 @@ const addMessage = (message, idType) => {
     //Create an "li" node:
     const node = document.createElement("li");
 
+    // Create img node
+    const img = document.createElement("img");
+    img.src = idType === "receivedMessage" ? "assets/icons/received_icon.svg": "assets/icons/sent_icon.svg";
+    img.classList.add('icon-list');
+
     // Create a text node:
-    const textnode = document.createTextNode(message);
+    const textnode = document.createElement("span");
+    textnode.textContent = message;
 
     // Append the text node to the "li" node:
+    node.appendChild(img);
     node.appendChild(textnode);
 
     // Append the "li" node to the list:
@@ -30,17 +37,52 @@ const addMessage = (message, idType) => {
 }
 
 let textMessage;
+const sendButton = document.getElementById("btnEnviar");
+sendButton.disabled = true;
+const textArea = document.getElementById("sMessage");
+textArea.onkeyup = function() {
+    console.log('focus');
+    // Do something while the textarea being used by a user
+    textMessage =  textArea.value;
+    if(textMessage) {
+        console.log('texto: ', textMessage);
+        sendButton.disabled = false;
+    }
+    
+};
+
 
 const sendMessage = () => {
     console.log('click on button');
-    textMessage =  document.getElementById("sMessage").value;
+    // textMessage =  textArea.value;
+    
 
     // verificar primero que esté conectado
     console.log('textMessage: ', textMessage);
-    // Enviar mensaje
-    client.publish('salida', textMessage || 'Mensaje de prueba', error => {
-        console.log(error || 'Mensaje enviado!!!');
-    });
+    console.log('client: ', client.connected);
+    sendButton.disabled = !!textMessage;
+    addMessage(textMessage, 'sendMessage');
+    if(!textMessage){
+        alert('Escriba un mensaje');
+    }
+
+    if(client.connected) {
+        // Enviar mensaje
+        client.publish('salida', textMessage || 'Mensaje de prueba', error => {
+
+            if(error) {
+                alert('Su mensaje no pudo ser enviado', error);
+                console.log(error);
+            } else {
+                console.log('Mensaje enviado!!!');
+                addMessage(textMessage, 'sendMessage');
+            }
+            
+        });
+    } else {
+        alert('El cliente no está conectado');
+    }
+   
 
 }
 
